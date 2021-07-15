390e35a25f4feea339a0870b25eb3ebed965f976
Sam Crouch <stcrouch17@gmail.com>
2021-07-15 11:17:08 -1000

    fixed

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
-
+const port = process.env.PORT || 80;
 
 app.use(express.json())
 
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-// app.listen(port, () => console.log(`Example app listening at nba-wiki-sam.herokuapp.com`))
+app.listen(port, () => console.log(`Example app listening at nba-wiki-sam.herokuapp.com`))
