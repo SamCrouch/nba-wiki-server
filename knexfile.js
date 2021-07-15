d169668d452fdec686b47d20d86c36744e5a27d5
Sam Crouch <stcrouch17@gmail.com>
2021-07-15 12:04:21 -1000

    server update

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,7 @@
 // Update with your config settings.
 
-const connectionString = process.env.DATABASE_URL;
+const connectionString = 'postgres://urtyetkjmnxoxb:7f9ef658bffdc7941bf5c46b7909ed2641a41541f888e89baa68ce034dd0e932@ec2-52-86-25-51.compute-1.amazonaws.com:5432/d601e5a7lh2059'
+
 
 module.exports = {
 
